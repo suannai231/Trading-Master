7a14dc328a1e7373a1af4769ff6e2197ae827c16
Jack Yin <suannai231@msn.com>
2021-10-30 13:23:01 -0500

    c

--- One/screen_One_2.py
+++ One/screen_One_2.py
@@ -38,6 +38,25 @@ WR120_GREATER_THAN_50_DAYS_75 = 182.1583333
 WR120_GREATER_THAN_80_DAYS_25 = 102.0666667
 WR120_GREATER_THAN_80_DAYS_75 = 108.5083333
 
+TURN_MIN = 0.083459226
+TURN_MAX = 2.113499411
+OBV_ABOVE_ZERO_DAYS_MIN = 102.6
+OBV_ABOVE_ZERO_DAYS_MAX = 160.4666667
+OBV_DIFF_RATE_MIN = 0.382331379
+OBV_DIFF_RATE_MAX = 0.526553412
+CUM_TURNOVER_RATE_MIN = 5.526716335
+CUM_TURNOVER_RATE_MAX = 17.13358213
+# cum_chip_bar = 0.8
+# chip_concentration_bar = 0.4
+WR34_MIN = 20.77973946
+WR34_MAX = 38.08395473
+WR120_MIN = 42.96485185
+WR120_MAX = 56.23581796
+WR120_GREATER_THAN_50_DAYS_MIN = 165.3
+WR120_GREATER_THAN_50_DAYS_MAX = 184.8666667
+WR120_GREATER_THAN_80_DAYS_MIN = 94.4
+WR120_GREATER_THAN_80_DAYS_MAX = 111.7
+
 def screen(df):
     if len(df) <= backward+2:
         return pd.DataFrame()
@@ -57,20 +76,20 @@ def screen(df):
     if MACD_Indicator & ((df['MACD_dif'][lastindex]-df['MACD_dea'][lastindex]) < 0):
         return pd.DataFrame()
 
-    if turn_Indicator & ((df['turn'][lastindex] > TURN_75) | (df['turn'][lastindex] < TURN_25)):
+    if turn_Indicator & ((df['turn'][lastindex] > TURN_MAX) | (df['turn'][lastindex] < TURN_MIN)):
         return pd.DataFrame()
 
     obv_above_zero_days = 0
     for i in range(startindex,endindex):
         if df['OBV_DIFF'][i] > 0:
             obv_above_zero_days += 1
-    if OBV_Indicator & (obv_above_zero_days > OBV_ABOVE_ZERO_DAYS_75) | (obv_above_zero_days < OBV_ABOVE_ZERO_DAYS_25):
+    if OBV_Indicator & (obv_above_zero_days > OBV_ABOVE_ZERO_DAYS_MAX) | (obv_above_zero_days < OBV_ABOVE_ZERO_DAYS_MIN):
         return pd.DataFrame()
 
-    if OBV_Indicator & ((df['OBV_DIFF_RATE'][lastindex] > OBV_DIFF_RATE_75) | (df['OBV_DIFF_RATE'][lastindex] < OBV_DIFF_RATE_25)):
+    if OBV_Indicator & ((df['OBV_DIFF_RATE'][lastindex] > OBV_DIFF_RATE_MAX) | (df['OBV_DIFF_RATE'][lastindex] < OBV_DIFF_RATE_MIN)):
         return pd.DataFrame()
 
-    if Cum_Turnover_Indicator & ((df['cum_turnover'][lastindex] > CUM_TURNOVER_RATE_75) | (df['cum_turnover'][lastindex] < CUM_TURNOVER_RATE_25)):
+    if Cum_Turnover_Indicator & ((df['cum_turnover'][lastindex] > CUM_TURNOVER_RATE_MAX) | (df['cum_turnover'][lastindex] < CUM_TURNOVER_RATE_MIN)):
         return pd.DataFrame()
 
     # ss = chip.Cal_Chip_Distribution(df)
@@ -82,24 +101,24 @@ def screen(df):
     #     if Chip_Concentration_Indicator & (chip_con > chip_concentration_bar):
     #         return pd.DataFrame()
 
-    if WR_Indicator & ((df.WR34[lastindex] > WR34_75) | (df.WR34[lastindex] < WR34_25)):
+    if WR_Indicator & ((df.WR34[lastindex] > WR34_MAX) | (df.WR34[lastindex] < WR34_MIN)):
         return pd.DataFrame()
 
-    if WR_Indicator & ((df.WR34[lastindex] > WR120_75) | (df.WR34[lastindex] < WR120_25)):
+    if WR_Indicator & ((df.WR120[lastindex] > WR120_MAX) | (df.WR120[lastindex] < WR120_MIN)):
         return pd.DataFrame()
     
     wr120_less_than_50_days = 0
     for i in range(startindex,endindex):
         if df.WR120[i] > 50:
             wr120_less_than_50_days += 1
-    if WR_Indicator & ((wr120_less_than_50_days > WR120_GREATER_THAN_50_DAYS_75) | (wr120_less_than_50_days < WR120_GREATER_THAN_50_DAYS_25)):
+    if WR_Indicator & (wr120_less_than_50_days < WR120_GREATER_THAN_50_DAYS_MIN):
         return pd.DataFrame()
 
     wr120_less_than_80_days = 0
     for i in range(startindex,endindex):
         if df.WR120[i] > 80:
             wr120_less_than_80_days += 1
-    if WR_Indicator & ((wr120_less_than_80_days > WR120_GREATER_THAN_80_DAYS_75) | (wr120_less_than_80_days < WR120_GREATER_THAN_80_DAYS_25)):
+    if WR_Indicator & (wr120_less_than_80_days < WR120_GREATER_THAN_80_DAYS_MIN):
         return pd.DataFrame()
 
     return df[df.index==df.index[-1]]
@@ -111,24 +130,31 @@ def screen(df):
 #         save.to_csv(screened_data_path+'/'+file)
 #     return file
 
-def run(df,date_chunk):
-    tickers_dates_df = pd.DataFrame()
-    for date in date_chunk:
-        tickers = df[df['date']==str(date)].ticker.unique()
-        if len(tickers) == 0:
+def run(ticker_chunk_df):
+    if ticker_chunk_df.empty:
+        return pd.DataFrame()
+    tickers = ticker_chunk_df.ticker.unique()
+    if len(tickers) == 0:
+        return pd.DataFrame()
+    return_ticker_chunk_df = pd.DataFrame()
+    for ticker in tickers:
+        ticker_df = ticker_chunk_df[ticker_chunk_df.ticker==ticker].reset_index(drop=True)
+        if ticker_df.empty:
             continue
-        date_df = df[df['date'].isin(pd.date_range(end=str(date), periods=300))]
-        tickers_date_df = pd.DataFrame()
-        for ticker in tickers:
-            ticker_date_df = date_df[date_df.ticker == ticker].reset_index(drop=True)
-            if (len(ticker_date_df)) <= backward+2:
+        dates = ticker_df.date.unique()
+        if len(dates) == 0:
+            continue
+        return_ticker_df = pd.DataFrame()
+        for date in dates:
+            date_ticker_df = ticker_df[ticker_df['date'].isin(pd.date_range(end=str(date), periods=300))].reset_index(drop=True)
+            if (len(date_ticker_df)) <= backward+2:
                 continue
-            result = screen(ticker_date_df)
+            result = screen(date_ticker_df)
             if not result.empty:
-                tickers_date_df = tickers_date_df.append(result,ignore_index=True)
-        if not tickers_date_df.empty:
-            tickers_dates_df = tickers_dates_df.append(tickers_date_df,ignore_index=True)
-    return tickers_dates_df
+                return_ticker_df = return_ticker_df.append(result,ignore_index=True)
+        if not return_ticker_df.empty:
+            return_ticker_chunk_df = return_ticker_chunk_df.append(return_ticker_df,ignore_index=True)
+    return return_ticker_chunk_df
 
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
@@ -150,18 +176,15 @@ if __name__ == '__main__':
         exit()
 
     df = pd.read_feather(processed_data_path + f'{end}' + '.feather')
-
-    date_list = [end - datetime.timedelta(days=x) for x in range(1,run_days)]
+    tickers = df.ticker.unique()
 
     cores = multiprocessing.cpu_count()
-    date_chunk_list = list(chunks(date_list,int(len(date_list)/cores)))
+    ticker_chunk_list = list(chunks(tickers,int(len(tickers)/cores)))
     pool=Pool(cores)
     async_results = []
-    for date_chunk in date_chunk_list:
-        last_date_in_date_chunk = date_chunk[-1]
-        extend_date_list = pd.date_range(end=str(last_date_in_date_chunk), periods=300)
-        date_chunk_df = df[df['date'].isin(date_chunk) | df['date'].isin(extend_date_list)]
-        async_result = pool.apply_async(run, args=(date_chunk_df,date_chunk))
+    for ticker_chunk in ticker_chunk_list:
+        ticker_chunk_df = df[df['ticker'].isin(ticker_chunk)]
+        async_result = pool.apply_async(run, args=(ticker_chunk_df,))
         async_results.append(async_result)
     pool.close()
     del(df)
@@ -171,4 +194,4 @@ if __name__ == '__main__':
         if not result.empty:
             df = df.append(result,ignore_index=True)
     df.to_csv(screened_data_path + f'{end}' + '.csv')
-    os.popen(f'python C:/Code/One/find_topX_MP_One.py')
+    # os.popen(f'python C:/Code/One/find_topX_MP_One.py')
