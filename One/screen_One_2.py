9945b966be0c1aef50113133d27ea07319b40582
Jack Yin <suannai231@msn.com>
2021-10-31 23:49:48 -0500

    c

--- One/screen_One_2.py
+++ One/screen_One_2.py
@@ -1,97 +1,115 @@
 import multiprocessing
-from yahoo_fin import stock_info as si
+# from yahoo_fin import stock_info as si
 import pandas as pd
-import numpy as np
+# import numpy as np
 import datetime
 import os
 # import chip
 from multiprocessing import Pool
+import time
 
-run_days = 100
-backward = 200
+# run_days = 100
+# backward = 200
 
-EMA_Indicator = True
-MACD_Indicator = True
-turn_Indicator = True
-OBV_Indicator = True
-Cum_Turnover_Indicator = True
-# Cum_Chip_Indicator = False
-Chip_Concentration_Indicator = False
-WR_Indicator = True
+CLOSE_ABOVE_EMA = True
+MACD_DIF_ABOVE_MACD_DEA = True
+TURN_MINMAX = False
+TURN_2575 = True
+OBV_ABOVE_ZERO_DAYS_MINMAX = False
+OBV_ABOVE_ZERO_DAYS_2575 = True
+OBV_DIFF_RATE_MINMAX = False
+OBV_DIFF_RATE_2575 = True
+CUM_TURNOVER_RATE_MINMAX = False
+CUM_TURNOVER_RATE_2575 = True
+WR34_MINMAX = False
+WR34_2575 = True
+WR120_MINMAX = False
+WR120_2575 = True
+WR120_50_MINMAX = False
+WR120_50_2575 = True
+WR120_80_MINMAX = False
+WR120_80_2575 = True
 
-TURN_25 = 0.163380811
-TURN_75 = 0.313519497
-OBV_ABOVE_ZERO_DAYS_25 = 111.0916667
-OBV_ABOVE_ZERO_DAYS_75 = 132.85
-OBV_DIFF_RATE_25 = 0.456246207
-OBV_DIFF_RATE_75 = 0.494789431
-CUM_TURNOVER_RATE_25 = 6.427935431
-CUM_TURNOVER_RATE_75 = 8.449866258
+TURN_25 = 0.001843759
+TURN_75 = 0.016290666
+OBV_ABOVE_ZERO_DAYS_25 = 90
+OBV_ABOVE_ZERO_DAYS_75 = 174
+OBV_DIFF_RATE_25 = 0.031796562
+OBV_DIFF_RATE_75 = 0.34831409
+CUM_TURNOVER_RATE_25 = 2.671338854
+CUM_TURNOVER_RATE_75 = 7.78641416
 # cum_chip_bar = 0.8
 # chip_concentration_bar = 0.4
-WR34_25 = 23.82415292
-WR34_75 = 31.02580587
-WR120_25 = 49.17431116
-WR120_75 = 52.59873443
-WR120_GREATER_THAN_50_DAYS_25 = 174.95
-WR120_GREATER_THAN_50_DAYS_75 = 182.1583333
-WR120_GREATER_THAN_80_DAYS_25 = 102.0666667
-WR120_GREATER_THAN_80_DAYS_75 = 108.5083333
+WR34_25 = 40.75829257
+WR34_75 = 82.56881261
+WR120_25 = 69.51219738
+WR120_75 = 91.76470544
+WR120_GREATER_THAN_50_DAYS_25 = 166
+WR120_GREATER_THAN_50_DAYS_75 = 197
+WR120_GREATER_THAN_80_DAYS_25 = 64
+WR120_GREATER_THAN_80_DAYS_75 = 150
 
-TURN_MIN = 0.083459226
-TURN_MAX = 2.113499411
-OBV_ABOVE_ZERO_DAYS_MIN = 102.6
-OBV_ABOVE_ZERO_DAYS_MAX = 160.4666667
-OBV_DIFF_RATE_MIN = 0.382331379
-OBV_DIFF_RATE_MAX = 0.526553412
-CUM_TURNOVER_RATE_MIN = 5.526716335
-CUM_TURNOVER_RATE_MAX = 17.13358213
+TURN_MIN = 0.023769694
+TURN_MAX = 4.854261389
+OBV_ABOVE_ZERO_DAYS_MIN = 8
+OBV_ABOVE_ZERO_DAYS_MAX = 200
+OBV_DIFF_RATE_MIN = 0.000140247
+OBV_DIFF_RATE_MAX = 1
+CUM_TURNOVER_RATE_MIN = 1.200638886
+CUM_TURNOVER_RATE_MAX = 77.99560789
 # cum_chip_bar = 0.8
 # chip_concentration_bar = 0.4
-WR34_MIN = 20.77973946
-WR34_MAX = 38.08395473
-WR120_MIN = 42.96485185
-WR120_MAX = 56.23581796
-WR120_GREATER_THAN_50_DAYS_MIN = 165.3
-WR120_GREATER_THAN_50_DAYS_MAX = 184.8666667
-WR120_GREATER_THAN_80_DAYS_MIN = 94.4
-WR120_GREATER_THAN_80_DAYS_MAX = 111.7
+WR34_MIN = 0
+WR34_MAX = 100
+WR120_MIN = 0.757581046
+WR120_MAX = 100
+WR120_GREATER_THAN_50_DAYS_MIN = 60
+WR120_GREATER_THAN_50_DAYS_MAX = 200
+WR120_GREATER_THAN_80_DAYS_MIN = 12
+WR120_GREATER_THAN_80_DAYS_MAX = 200
 
 def screen(df):
-    if len(df) <= backward+2:
-        return pd.DataFrame()
+    # if len(df) <= backward+2:
+    #     return pd.DataFrame()
 
-    backward_startindex = len(df)-backward
-    origin_endindex = len(df)
+    # backward_startindex = len(df)-backward
+    # origin_endindex = len(df)
     
-    df = df[backward_startindex:origin_endindex].reset_index(drop=True)
+    # df = df[backward_startindex:origin_endindex].reset_index(drop=True)
 
-    startindex = df.index[0]
-    endindex = len(df)
+    # startindex = df.index[0]
+    # endindex = len(df)
     lastindex = df.index[-1]
 
-    if EMA_Indicator & (df['close'][lastindex] < df['EMA34'][lastindex]):
+    if CLOSE_ABOVE_EMA & (df['close'][lastindex] < df['EMA34'][lastindex]):
         return pd.DataFrame()
 
-    if MACD_Indicator & ((df['MACD_dif'][lastindex]-df['MACD_dea'][lastindex]) < 0):
+    if MACD_DIF_ABOVE_MACD_DEA & ((df['MACD_dif'][lastindex]-df['MACD_dea'][lastindex]) < 0):
         return pd.DataFrame()
 
-    if turn_Indicator & ((df['turn'][lastindex] > TURN_MAX) | (df['turn'][lastindex] < TURN_MIN)):
+    if TURN_MINMAX & ((df['turn'][lastindex] > TURN_MAX) | (df['turn'][lastindex] < TURN_MIN)):
+        return pd.DataFrame()
+    if TURN_2575 & ((df['turn'][lastindex] > TURN_75) | (df['turn'][lastindex] < TURN_25)):
         return pd.DataFrame()
 
-    obv_above_zero_days = 0
-    for i in range(startindex,endindex):
-        if df['OBV_DIFF'][i] > 0:
-            obv_above_zero_days += 1
-    if OBV_Indicator & (obv_above_zero_days > OBV_ABOVE_ZERO_DAYS_MAX) | (obv_above_zero_days < OBV_ABOVE_ZERO_DAYS_MIN):
+    # obv_above_zero_days = 0
+    # for i in range(startindex,endindex):
+    #     if df['OBV_DIFF'][i] > 0:
+    #         obv_above_zero_days += 1
+    if OBV_ABOVE_ZERO_DAYS_MINMAX & (df['turn']['obv_above_zero_days'] > OBV_ABOVE_ZERO_DAYS_MAX) | (df['turn']['obv_above_zero_days'] < OBV_ABOVE_ZERO_DAYS_MIN):
+        return pd.DataFrame()
+    if OBV_ABOVE_ZERO_DAYS_2575 & (df['turn']['obv_above_zero_days'] > OBV_ABOVE_ZERO_DAYS_75) | (df['turn']['obv_above_zero_days'] < OBV_ABOVE_ZERO_DAYS_25):
         return pd.DataFrame()
 
-    if OBV_Indicator & ((df['OBV_DIFF_RATE'][lastindex] > OBV_DIFF_RATE_MAX) | (df['OBV_DIFF_RATE'][lastindex] < OBV_DIFF_RATE_MIN)):
+    if OBV_DIFF_RATE_MINMAX & ((df['OBV_DIFF_RATE'][lastindex] > OBV_DIFF_RATE_MAX) | (df['OBV_DIFF_RATE'][lastindex] < OBV_DIFF_RATE_MIN)):
+        return pd.DataFrame()
+    if OBV_DIFF_RATE_2575 & ((df['OBV_DIFF_RATE'][lastindex] > OBV_DIFF_RATE_75) | (df['OBV_DIFF_RATE'][lastindex] < OBV_DIFF_RATE_25)):
         return pd.DataFrame()
 
-    if Cum_Turnover_Indicator & ((df['cum_turnover'][lastindex] > CUM_TURNOVER_RATE_MAX) | (df['cum_turnover'][lastindex] < CUM_TURNOVER_RATE_MIN)):
+    if CUM_TURNOVER_RATE_MINMAX & ((df['cum_turnover'][lastindex] > CUM_TURNOVER_RATE_MAX) | (df['cum_turnover'][lastindex] < CUM_TURNOVER_RATE_MIN)):
         return pd.DataFrame()
-
+    if CUM_TURNOVER_RATE_2575 & ((df['cum_turnover'][lastindex] > CUM_TURNOVER_RATE_75) | (df['cum_turnover'][lastindex] < CUM_TURNOVER_RATE_25)):
+        return pd.DataFrame()
     # ss = chip.Cal_Chip_Distribution(df)
     # if not ss.empty:
     #     cum_chip = ss['Cum_Chip'][ss.index[-1]]
@@ -101,27 +119,35 @@ def screen(df):
     #     if Chip_Concentration_Indicator & (chip_con > chip_concentration_bar):
     #         return pd.DataFrame()
 
-    if WR_Indicator & ((df.WR34[lastindex] > WR34_MAX) | (df.WR34[lastindex] < WR34_MIN)):
+    if WR34_MINMAX & ((df.WR34[lastindex] > WR34_MAX) | (df.WR34[lastindex] < WR34_MIN)):
+        return pd.DataFrame()
+    if WR34_2575 & ((df.WR34[lastindex] > WR34_75) | (df.WR34[lastindex] < WR34_25)):
         return pd.DataFrame()
 
-    if WR_Indicator & ((df.WR120[lastindex] > WR120_MAX) | (df.WR120[lastindex] < WR120_MIN)):
+    if WR120_MINMAX & ((df.WR120[lastindex] > WR120_MAX) | (df.WR120[lastindex] < WR120_MIN)):
         return pd.DataFrame()
-    
-    wr120_less_than_50_days = 0
-    for i in range(startindex,endindex):
-        if df.WR120[i] > 50:
-            wr120_less_than_50_days += 1
-    if WR_Indicator & (wr120_less_than_50_days < WR120_GREATER_THAN_50_DAYS_MIN):
+    if WR120_2575 & ((df.WR120[lastindex] > WR120_75) | (df.WR120[lastindex] < WR120_25)):
         return pd.DataFrame()
 
-    wr120_less_than_80_days = 0
-    for i in range(startindex,endindex):
-        if df.WR120[i] > 80:
-            wr120_less_than_80_days += 1
-    if WR_Indicator & (wr120_less_than_80_days < WR120_GREATER_THAN_80_DAYS_MIN):
+    # wr120_less_than_50_days = 0
+    # for i in range(startindex,endindex):
+    #     if df.WR120[i] > 50:
+    #         wr120_less_than_50_days += 1
+    if WR120_50_MINMAX & (df['turn']['wr120_less_than_50_days'] < WR120_GREATER_THAN_50_DAYS_MIN) | (df['turn']['wr120_less_than_50_days'] > WR120_GREATER_THAN_50_DAYS_MAX):
+        return pd.DataFrame()
+    if WR120_50_2575 & (df['turn']['wr120_less_than_50_days'] < WR120_GREATER_THAN_50_DAYS_25) | (df['turn']['wr120_less_than_50_days'] > WR120_GREATER_THAN_50_DAYS_75):
         return pd.DataFrame()
 
-    return df[df.index==df.index[-1]]
+    # wr120_less_than_80_days = 0
+    # for i in range(startindex,endindex):
+    #     if df.WR120[i] > 80:
+    #         wr120_less_than_80_days += 1
+    if WR120_80_MINMAX & (df['turn']['wr120_less_than_80_days'] < WR120_GREATER_THAN_80_DAYS_MIN) | (df['turn']['wr120_less_than_80_days'] > WR120_GREATER_THAN_80_DAYS_MAX):
+        return pd.DataFrame()
+    if WR120_80_2575 & (df['turn']['wr120_less_than_80_days'] < WR120_GREATER_THAN_50_DAYS_25) | (df['turn']['wr120_less_than_80_days'] > WR120_GREATER_THAN_80_DAYS_75):
+        return pd.DataFrame()
+
+    return df
 
 # def run(file):                   #把要执行的代码写到run函数里面 线程在创建后会直接运行run函数 
 #     df = pd.read_csv(processed_data_path+'/'+file)
@@ -138,20 +164,20 @@ def run(ticker_chunk_df):
         return pd.DataFrame()
     return_ticker_chunk_df = pd.DataFrame()
     for ticker in tickers:
-        ticker_df = ticker_chunk_df[ticker_chunk_df.ticker==ticker].reset_index(drop=True)
+        ticker_df = ticker_chunk_df[ticker_chunk_df.ticker==ticker].set_index('date')
         if ticker_df.empty:
             continue
         dates = ticker_df.date.unique()
         if len(dates) == 0:
             continue
         return_ticker_df = pd.DataFrame()
+        start_time = time.time()
         for date in dates:
-            date_ticker_df = ticker_df[ticker_df['date'].isin(pd.date_range(end=str(date), periods=300))].reset_index(drop=True)
-            if (len(date_ticker_df)) <= backward+2:
-                continue
+            date_ticker_df = ticker_df[date]
             result = screen(date_ticker_df)
             if not result.empty:
                 return_ticker_df = return_ticker_df.append(result,ignore_index=True)
+        print("%s seconds\n" %(time.time()-start_time))
         if not return_ticker_df.empty:
             return_ticker_chunk_df = return_ticker_chunk_df.append(return_ticker_df,ignore_index=True)
     return return_ticker_chunk_df
@@ -162,7 +188,7 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 end = datetime.date.today()
-processed_data_path=f"//jack-nas/Work/Python/ProcessedData/"
+processed_data_path=f"//jack-nas/Work/Python/SecretNum/"
 screened_data_path=f"//jack-nas/Work/Python/ScreenedData/"
 
 if __name__ == '__main__':
