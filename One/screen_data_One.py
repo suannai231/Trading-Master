ed6c704a187ab2c9ce06362c606079a147c6adf2
Jack Yin <suannai231@gmail.com>
2022-03-15 23:38:08 -0500

    c

--- One/screen_data_One.py
+++ One/screen_data_One.py
@@ -72,12 +72,13 @@ def screen(df):
     close = df.iloc[-1]['close']
     last_ema20 = df.iloc[-1]['EMA20']
     volume = df.iloc[-1]['volume']
+    turn = df.iloc[-1]['turn']
 
     close2 = df.iloc[-2]['close']
     last2_ema20 = df.iloc[-2]['EMA20']
     volume2 = df.iloc[-2]['volume']
 
-    if (close2<=last2_ema20) and (close>=last_ema20) and (volume >= volume2*3):
+    if (close2<=last2_ema20) and (close>=last_ema20) and (turn >= 0.1):
         return df.tail(1)
 
     return pd.DataFrame()
