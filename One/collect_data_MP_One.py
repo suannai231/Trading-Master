2daa14c3244d767dc8cc895e0878da127ff3ab64
Jack Yin <suannai231@msn.com>
2022-03-25 22:02:57 +0000

    c

--- One/collect_data_MP_One.py
+++ One/collect_data_MP_One.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
     tickers = nasdaq + other
 
     cores = multiprocessing.cpu_count()
-    ticker_chunk_list = list(chunks(tickers,int(len(tickers)/(cores*4))))
+    ticker_chunk_list = list(chunks(tickers,int(len(tickers)/(cores*3))))
     proc_num = len(ticker_chunk_list)
 
     # if qfq_file not in files:
