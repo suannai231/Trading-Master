4e7b342d3220cafb22af07390f1635c2568e6ae5
Jack Yin <suannai231@msn.com>
2022-07-24 01:49:06 -0500

    c

--- One/run.bat
+++ One/run.bat
@@ -10,5 +10,5 @@ set FILENAME=%CUR_YYYY%%CUR_MM%%CUR_DD%-%CUR_HH%%CUR_NN%%CUR_SS%
 
 call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
 cd C:\Code\One
-python process_data_MP_One.py>C:\Python\%FILENAME%.txt
+python collect_data_MP_One.py>C:\Python\%FILENAME%.txt
 exit
