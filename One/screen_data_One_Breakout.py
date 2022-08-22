77c0150247c74d0b60335a42dfe0d570e61cba25
Jack Yin <suannai231@msn.com>
2022-08-22 14:22:58 -0500

    c

--- One/screen_data_One_Breakout.py
+++ One/screen_data_One_Breakout.py
@@ -66,7 +66,10 @@ def screen(df,lines):
         #     logging.error(ticker + "52 week range is nan.")
         #     return False
         # if (Year_Low*2.5>=close>=Year_Low*1.2) & (close>=(Year_Low+Year_High)/2):
-        long_trend_days = len(df.loc[(df.EMA20>=df.EMA60) & (df.EMA60>=df.EMA120)])
+        if(len(df)<60):
+            return False
+        last_60days_df = df.iloc[len(df)-60:]
+        long_trend_days = len(last_60days_df.loc[(last_60days_df.EMA20>=last_60days_df.EMA60) & (last_60days_df.EMA60>=last_60days_df.EMA120)])
         if(close >= Vol_High_Price*0.8) & (ema20>=EMA20_High*0.8) & (long_trend_days>=60):
             return True
         else:
