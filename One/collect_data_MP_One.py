c1b5394f6fa0d4e53b65638636ccf4849c6b20e4
Jack Yin <suannai231@msn.com>
2022-08-26 11:29:22 -0500

    c

--- One/collect_data_MP_One.py
+++ One/collect_data_MP_One.py
@@ -220,7 +220,7 @@ if __name__ == '__main__':
             continue
         tickers = nasdaq + other
 
-    cores = multiprocessing.cpu_count()
+    cores = int(multiprocessing.cpu_count()/2)
     ticker_chunk_list = list(chunks(tickers,math.ceil(len(tickers)/(cores))))
     proc_num = len(ticker_chunk_list)
 
