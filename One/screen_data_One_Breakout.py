2a5c0a1bff7aad16aa8f995b173a70e5cba68505
Jack Yin <suannai231@msn.com>
2022-09-02 13:58:43 -0500

    c

--- One/screen_data_One_Breakout.py
+++ One/screen_data_One_Breakout.py
@@ -35,7 +35,7 @@ def screen(df,lines):
     change = df.iloc[-1]['change']
 
     if lines=="Close to EMA20":
-        if(ema20 <= close <= ema20*1.1):
+        if(ema20 <= close <= ema20*1.2):
             return True
         else:
             return False
