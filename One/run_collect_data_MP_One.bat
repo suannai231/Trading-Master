4c6539fe8ecf6f2d0b3c0ee607389cc2dd071e13
Jack Yin <suannai231@msn.com>
2022-12-13 10:19:19 -0600

    c

--- One/run_collect_data_MP_One.bat
+++ One/run_collect_data_MP_One.bat
@@ -2,5 +2,5 @@ call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
 cd C:\Code\One
 powercfg /setactive e9a42b02-d5df-448d-aa00-03f14749eb61
 python collect_data_MP_One.py
-powercfg /setactive a1841308-3541-4fab-bc81-f71556f20b4a
+powercfg /setactive 381b4222-f694-41f0-9685-ff5bb260df2e
 exit
