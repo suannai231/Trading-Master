afc147283343db828ede18415dc14366b8bc7ca9
Jack Yin <suannai231@msn.com>
2023-02-09 12:03:54 -0600

    c

--- Tensor/test.py
+++ Tensor/test.py
@@ -1,77 +1,77 @@
-import os
 import numpy as np
 import pandas as pd
-import tensorflow as tf
 import matplotlib.pyplot as plt
+import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM
+import os
+import math
+from tensorflow import keras
+from tensorflow.keras import layers
 
-# print("TensorFlow version:", tf.__version__)
-# print("cuDNN enabled:", tf.test.is_built_with_cuda())
 
-# # os.environ["CUDA_VISIBLE_DEVICES"] = "1" # change 0 to the number of the GPU you want to use
-# print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-
-# from tensorflow.compat.v1 import GPUOptions
-# from tensorflow.compat.v1 import ConfigProto
-
-# config = ConfigProto()
-# config.gpu_options.allow_growth = True
-# config.gpu_options.per_process_gpu_memory_fraction = 0.9
-
-# Load the stock data
-# df = pd.read_csv('stock_data.csv')
 raw_data_path='//jack-nas.home/Work/Python/RawData/'
 raw_data_files = os.listdir(raw_data_path)
 df = pd.read_feather(raw_data_path + raw_data_files[-1])
 # Prepare the data for modeling
-df = df[df.ticker=="UBX"]
-data = df.set_index("date",drop=True)
-data = data.drop(columns=["ticker"])
-# Load the stock data and drop any rows with missing values
-# data = pd.read_csv('stock_data.csv').dropna()
+df = df[df.ticker=="MARK"]
+df = df.set_index("date",drop=True)
+df = df.drop(columns=["ticker"])
 
-# Normalize the data using MinMaxScaler
-scaler = MinMaxScaler()
-data = scaler.fit_transform(data)
+features = df[['close','volume']]
+values = features.values
+training_data_len = math.ceil(len(values)* 0.8)
 
-# Split the data into training and test sets
-train_data = data[:int(data.shape[0] * 0.8), :]
-test_data = data[int(data.shape[0] * 0.8):, :]
+scaler = MinMaxScaler(feature_range=(0,1))
+# scaled_data = scaler.fit_transform(values.reshape(-1,1))
+scaled_data = scaler.fit_transform(values)
+train_data = scaled_data[0: training_data_len, :]
 
-# Create a function to generate time-step data for the LSTM model
-def generate_data(data, time_step):
-    X, Y = [], []
-    for i in range(data.shape[0] - time_step - 1):
-        X.append(data[i: i + time_step, :])
-        Y.append(data[i + time_step, -1])
-    return np.array(X), np.array(Y)
+x_train = []
+y_train = []
 
-time_step = 30
-X_train, Y_train = generate_data(train_data, time_step)
-X_test, Y_test = generate_data(test_data, time_step)
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60:i, 0])
+    y_train.append(train_data[i, 0])
+    
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
-# Define the LSTM model
-model = Sequential()
-model.add(LSTM(units=128, input_shape=(X_train.shape[1], X_train.shape[2])))
-model.add(Dense(1))
+test_data = scaled_data[training_data_len-60: , : ]
+x_test = []
+y_test = values[training_data_len:]
 
-# Compile the model and set the optimizer to use GPU acceleration
-model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.RMSprop(0.001))
+for i in range(60, len(test_data)):
+  x_test.append(test_data[i-60:i, 0])
 
-# Train the model on the training data
-model.fit(X_train, Y_train, epochs=100, batch_size=32, verbose=2)
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
-# Evaluate the model on the test data
-score = model.evaluate(X_test, Y_test)
-print(f'Test MSE: {score}')
+model = keras.Sequential()
+model.add(layers.LSTM(100, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(layers.LSTM(100, return_sequences=False))
+model.add(layers.Dense(25))
+model.add(layers.Dense(1))
+model.summary()
 
-# Make predictions on the test data
-predictions = model.predict(X_test)
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, batch_size= 1, epochs=3)
 
-# Plot the prediction results
-plt.plot(Y_test, label='True')
-plt.plot(predictions, label='Predicted')
-plt.legend()
+
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
+rmse = np.sqrt(np.mean(predictions - y_test)**2)
+rmse
+
+data = df.filter(['close'])
+train = data[:training_data_len]
+validation = data[training_data_len:]
+validation['Predictions'] = predictions
+plt.figure(figsize=(16,8))
+plt.title('Model')
+plt.xlabel('Date')
+plt.ylabel('Close Price USD ($)')
+# plt.plot(train)
+plt.plot(validation[['close', 'Predictions']])
+# plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
+plt.legend(['Val', 'Predictions'], loc='lower right')
 plt.show()
