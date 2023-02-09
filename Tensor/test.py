fd167296c0e0138521de0b49979758b5c9f48986
Jack Yin <suannai231@msn.com>
2023-02-09 12:35:30 -0600

    c

--- Tensor/test.py
+++ Tensor/test.py
@@ -7,6 +7,7 @@ import os
 import math
 from tensorflow import keras
 from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
 
 
 raw_data_path='//jack-nas.home/Work/Python/RawData/'
@@ -17,61 +18,42 @@ df = df[df.ticker=="MARK"]
 df = df.set_index("date",drop=True)
 df = df.drop(columns=["ticker"])
 
-features = df[['close','volume']]
-values = features.values
-training_data_len = math.ceil(len(values)* 0.8)
+data = df[['Close', 'Volume']]
 
-scaler = MinMaxScaler(feature_range=(0,1))
-# scaled_data = scaler.fit_transform(values.reshape(-1,1))
-scaled_data = scaler.fit_transform(values)
-train_data = scaled_data[0: training_data_len, :]
+# Normalize the values of the two features
+scaler = MinMaxScaler()
+data[['Close', 'Volume']] = scaler.fit_transform(data[['Close', 'Volume']])
 
-x_train = []
-y_train = []
+# Split the data into training, validation, and test sets
+train_data, test_data = train_test_split(data, test_size=0.2, shuffle=False)
+train_data, val_data = train_test_split(train_data, test_size=0.2, shuffle=False)
 
-for i in range(60, len(train_data)):
-    x_train.append(train_data[i-60:i, 0])
-    y_train.append(train_data[i, 0])
-    
-x_train, y_train = np.array(x_train), np.array(y_train)
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+# Convert the close prices into labels that can be used to train the neural network
+train_labels = train_data.pop('Close')
+val_labels = val_data.pop('Close')
+test_labels = test_data.pop('Close')
 
-test_data = scaled_data[training_data_len-60: , : ]
-x_test = []
-y_test = values[training_data_len:]
+# Define the neural network architecture
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(64, activation='relu', input_shape=(1,)),
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(1)
+])
 
-for i in range(60, len(test_data)):
-  x_test.append(test_data[i-60:i, 0])
-
-x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-model = keras.Sequential()
-model.add(layers.LSTM(100, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-model.add(layers.LSTM(100, return_sequences=False))
-model.add(layers.Dense(25))
-model.add(layers.Dense(1))
-model.summary()
-
+# Compile the model
 model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, batch_size= 1, epochs=3)
 
+# Train the model
+history = model.fit(train_data, train_labels, epochs=50, batch_size=32, validation_data=(val_data, val_labels))
 
-predictions = model.predict(x_test)
+# Make predictions on the test data
+predictions = model.predict(test_data)
+
+# Un-normalize the predicted values to get the actual close prices
 predictions = scaler.inverse_transform(predictions)
-rmse = np.sqrt(np.mean(predictions - y_test)**2)
-rmse
 
-data = df.filter(['close'])
-train = data[:training_data_len]
-validation = data[training_data_len:]
-validation['Predictions'] = predictions
-plt.figure(figsize=(16,8))
-plt.title('Model')
-plt.xlabel('Date')
-plt.ylabel('Close Price USD ($)')
-# plt.plot(train)
-plt.plot(validation[['close', 'Predictions']])
-# plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
-plt.legend(['Val', 'Predictions'], loc='lower right')
+# Plot the actual close prices and the predicted close prices
+plt.plot(range(len(test_labels)), test_labels, label='Actual Close Price')
+plt.plot(range(len(test_labels)), predictions, label='Predicted Close Price')
+plt.legend()
 plt.show()
