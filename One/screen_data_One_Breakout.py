d6c17c40196eabf26de44cda8705730df4c4b8a9
Jack Yin <suannai231@msn.com>
2023-04-03 11:09:20 -0500

    c

--- One/screen_data_One_Breakout.py
+++ One/screen_data_One_Breakout.py
@@ -71,14 +71,13 @@ def screen_data():
 
     processed_data_files = os.listdir(processed_data_path)
     if len(processed_data_files) == 0:
-        # log('warning',"processed data not ready, sleep 1 second...")
+        log('warning',"processed data not ready, sleep 1 second...")
         time.sleep(1)
         return
 
     screened_data_files = os.listdir(screened_data_path)
-    processed_data_files_str = processed_data_files[-1] + '.txt'
-    if processed_data_files_str in screened_data_files:
-        # log('warning',"warning: " + processed_data_files_str + " existed, sleep 10 second...")
+    if processed_data_files[-1] in screened_data_files:
+        log('warning',"warning: " + processed_data_files[-1] + " existed, sleep 10 second...")
         time.sleep(10)
         return
 
