e755a503444ab9512da2697c0a317e59b64c9792
Jack Yin <suannai231@msn.com>
2024-02-16 10:11:12 -0600

    Fix EMA20 calculation and add Ratio condition in screen function

--- One/screen_data_One_Breakout.py
+++ One/screen_data_One_Breakout.py
@@ -17,7 +17,7 @@ def screen(df):
     # change = df.iloc[-1].change >=0.05
 
     # EMA20
-    # EMA20 = df.iloc[-1].EMA20
+    EMA20 = df.iloc[-1].EMA20
     # EMA60
     EMA60 = df.iloc[-1].EMA60
     # DIS
@@ -26,10 +26,10 @@ def screen(df):
     DIS_REF = df.iloc[-2].DIS
     # DIS_EMA5
     DIS_EMA5 = df.iloc[-1].DIS_EMA5
-
+    Ratio = (close-EMA20)/EMA20
     # FLAG:= DIS>=DIS_EMA5 AND DIS>REF(DIS,1) AND DIS>=0
 
-    flag = DIS>=DIS_EMA5 and DIS>DIS_REF and close>EMA60 and close<10 and turnover_flag
+    flag = DIS>=DIS_EMA5 and DIS>DIS_REF and close>EMA60 and close<10 and turnover_flag and Ratio<0.2
 
     if df.iloc[-1].ticker == "NXT":
         log("info", df.iloc[-1].ticker)
