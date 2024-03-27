079a9da29c0f41f2be7cbd1fc056adddb31b745a
Jack Yin <suannai231@msn.com>
2024-03-27 14:57:37 -0500

    Update marketCapMax value

--- One/collect_data_MP_One.py
+++ One/collect_data_MP_One.py
@@ -19,7 +19,7 @@ from bs4 import BeautifulSoup
 days=365
 date_time = datetime.now()
 
-marketCapMax = 500000000
+marketCapMax = 1000000000
 marketCapMin = 1000000
 
 # regularMarketPreviousCloseMax = 20
